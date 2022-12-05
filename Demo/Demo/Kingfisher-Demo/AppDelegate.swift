0d60284d67dc2da17fd9f7998f58e277f6257a6e
Yamschikov-vs <yamschikov-vs@zvul.com>
2022-12-05 20:52:03 +0500

    test

--- Demo/Demo/Kingfisher-Demo/AppDelegate.swift
+++ Demo/Demo/Kingfisher-Demo/AppDelegate.swift
@@ -33,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // test
         return true
     }
     
